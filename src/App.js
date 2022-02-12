6e4b91173213e8838e96a1673ffa635e4d7052b3
Brienne Kordis <87245718+briennekordis@users.noreply.github.com>
2022-02-12 11:33:10 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Products from './cart.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Products />
+      </header>
+    </div>
+  );
+}
+
+export default App;
